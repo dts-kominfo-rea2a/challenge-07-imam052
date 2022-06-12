6c76ea42ed9e284d0d71558763f1f92e32e02f51
imam.arifin02 <imam.arifin052@gmail.com>
2022-06-12 21:50:59 +0700

    Tantangan Promise Done.

--- promise.js
+++ promise.js
@@ -1,8 +1,18 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
-// TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+//TODO: Buat fungsi promiseOutput sesuai ketentuan readme
+const promiseOutput = async (emosi) => {
+  let xxi = await promiseTheaterIXX();
+  let cgv = await promiseTheaterVGC();
 
+  // tampung semua array di variable cgv
+  xxi.forEach(element => {
+    cgv.push(element);
+  });
+  
+  const total = cgv.filter(a => a.hasil === emosi).length;
+  return total;
+}
 module.exports = {
   promiseOutput,
 };
